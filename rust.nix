ec9ec8503802c189d1acb2127da5a61e7542ed90
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-12-19 20:40:57 -0500

    init

---
+++ rust.nix
@@ -0,0 +1,29 @@
+{ pkgs, gitignore }:
+
+let
+  rustVersion = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml); # rust-bin.stable.latest.default
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = rustVersion;
+    rustc = rustVersion;
+  };
+  name = "tix";
+  version = "0.1.0";
+  rustBin = rustPlatform.buildRustPackage {
+    pname = name;
+    version = version;
+    src = gitignore.lib.gitignoreSource ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+    nativeBuildInputs = [ ];
+  };
+in
+{
+  rust-shell = (
+    rustVersion.override {
+      extensions = [
+        "rust-src"
+        "rust-analyzer"
+      ];
+    }
+  );
+  binary = rustBin;
+}
