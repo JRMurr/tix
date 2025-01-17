8520ac9d7586ec2de81b5e24d34733d63da1c168
John Murray <5672686+JRMurr@users.noreply.github.com>
2025-01-16 22:14:17 -0500

    split out collecting

---
+++ crates/lang/src/ty/check/collect.rs
@@ -0,0 +1,147 @@
+use std::collections::{BTreeMap, HashMap};
+
+use smol_str::SmolStr;
+
+use crate::ty::{ArcTy, AttrSetTy, Ty, TyRef};
+
+use super::{CheckCtx, InferenceResult, TyId};
+
+pub struct Collector<'db> {
+    cache: HashMap<TyId, ArcTy>,
+    ctx: CheckCtx<'db>,
+}
+
+impl<'db> Collector<'db> {
+    pub fn new(ctx: CheckCtx<'db>) -> Self {
+        Self {
+            cache: HashMap::with_capacity(ctx.table.len()),
+            ctx,
+        }
+    }
+
+    pub fn finalize_inference(&mut self) -> InferenceResult {
+        let ctx = &mut self.ctx;
+
+        let name_tys: Vec<_> = ctx
+            .module
+            .names()
+            .map(|(name, _)| {
+                // TODO: i think this is causing a new instantiation?
+                // let ty = ctx.ty_for_name(name);
+                // dbg!(&(name, name_txt));
+                let ty = u32::from(name.into_raw()).into();
+                (name, ty)
+            })
+            .collect();
+
+        let expr_tys: Vec<_> = ctx
+            .module
+            .exprs()
+            .map(|(expr, _)| {
+                let ty = ctx.ty_for_expr(expr);
+                (expr, ty)
+            })
+            .collect();
+
+        let name_cnt = ctx.module.names().len();
+        let expr_cnt = ctx.module.exprs().len();
+        let mut name_ty_map = HashMap::with_capacity(name_cnt);
+        let mut expr_ty_map = HashMap::with_capacity(expr_cnt);
+        for (name, ty) in name_tys {
+            name_ty_map.insert(name, self.canonicalize_type(ty));
+        }
+        for (expr, ty) in expr_tys {
+            expr_ty_map.insert(expr, self.canonicalize_type(ty));
+        }
+
+        // dbg!(&self.ctx.table);
+
+        InferenceResult {
+            name_ty_map,
+            expr_ty_map,
+        }
+    }
+
+    fn canonicalize_type(&mut self, ty: TyId) -> ArcTy {
+        let i = self.ctx.table.find(ty);
+        if let Some(ty) = self.cache.get(&i).cloned() {
+            return ty;
+        }
+
+        let ret = self.canonicalize_type_uncached(ty);
+        self.cache.insert(i, ret.clone());
+        ret
+    }
+
+    fn canonicalize_type_uncached(&mut self, ty_id: TyId) -> ArcTy {
+        // let ty = self.ctx.get_ty(ty_id);
+
+        let ty = self.ctx.table.inlined_probe_value(ty_id).known();
+
+        let ty = if let Some(t) = ty {
+            t
+        } else {
+            let root = self.ctx.table.find(ty_id);
+            // eprintln!("{ty_id:?} is unknown\troot: {root:?}");
+            Ty::TyVar(root.0)
+        };
+
+        match ty {
+            Ty::TyVar(x) => {
+                // TODO: this should just be a generic param at this point
+                // should eventually normalize the vars so they start from 0
+                // could maybe do that as a final pass on the name?
+                ArcTy::TyVar(x)
+                // if x != ty_id.0 {
+                //     self.canonicalize_type(TyId::from(x))
+                // } else {
+                //     ArcTy::TyVar(x)
+                // }
+            }
+            Ty::List(inner_id) => {
+                let c_inner = self.canonicalize_type(inner_id);
+                ArcTy::List(c_inner.into())
+            }
+            Ty::Lambda { param, body } => {
+                let c_param = self.canonicalize_type(param).into();
+                let c_body = self.canonicalize_type(body).into();
+                ArcTy::Lambda {
+                    param: c_param,
+                    body: c_body,
+                }
+            }
+            Ty::AttrSet(attr_set_ty) => {
+                // let c_attr = self.canonicalize_attrset(attr_set_ty);
+                // ArcTy::AttrSet(c_attr)
+                // ArcTy::AttrSet(AttrSetTy {
+                //     fields: BTreeMap::new(),
+                //     dyn_ty: None,
+                //     rest: None,
+                // })
+                self.canonicalize_attrset(attr_set_ty)
+            }
+            Ty::Primitive(p) => ArcTy::Primitive(p),
+        }
+    }
+
+    fn canonicalize_attrset(&mut self, attr_set_ty: AttrSetTy<TyId>) -> ArcTy {
+        let mut new_fields = BTreeMap::<SmolStr, TyRef>::new();
+        for (k, &v_id) in &attr_set_ty.fields {
+            let field_ty = self.canonicalize_type(v_id).into();
+            new_fields.insert(k.clone(), field_ty);
+        }
+        let c_dyn_ty = attr_set_ty
+            .dyn_ty
+            .map(|d_id| self.canonicalize_type(d_id).into());
+        // TODO: merge this in with fields
+        let c_rest = attr_set_ty
+            .rest
+            .map(|r_id| self.canonicalize_type(r_id).into());
+
+        ArcTy::AttrSet(AttrSetTy {
+            fields: new_fields,
+            dyn_ty: c_dyn_ty,
+            rest: c_rest,
+        })
+    }
+}
--- crates/lang/src/ty/check/mod.rs
+++ crates/lang/src/ty/check/mod.rs
@@ -1,10 +1,11 @@
+mod collect;
 mod generate;
 mod solve;
 
+use collect::Collector;
 use derive_more::Debug;
 use ena::unify::{self, InPlaceUnificationTable, UnifyKey, UnifyValue};
-use smol_str::SmolStr;
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 use thiserror::Error;
 
 use crate::{
@@ -13,12 +14,11 @@ use crate::{
     nameres::{DependentGroup, GroupedDefs, NameResolution},
 };
 
-use super::{ArcTy, AttrSetTy, Ty, TyRef};
+use super::{ArcTy, AttrSetTy, Ty};
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
 #[debug("TyId({_0:?})")]
 pub struct TyId(u32);
-// pub struct TyId(Id<Ty<TyId>>);
 
 impl From<u32> for TyId {
     #[inline]
@@ -33,26 +33,6 @@ impl From<usize> for TyId {
         (value as u32).into()
     }
 }
-
-// pub type Types = Arena<Ty<TyId>>;
-
-// impl From<Id<Ty<TyId>>> for TyId {
-//     fn from(value: Id<Ty<TyId>>) -> Self {
-//         Self(value)
-//     }
-// }
-
-// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-// pub struct TyIdEqKey {
-//     id: TyId,
-// }
-
-// impl From<TyId> for TyIdEqKey {
-//     #[inline]
-//     fn from(value: TyId) -> Self {
-//         Self { id: value }
-//     }
-// }
 
 #[derive(Clone, Debug)]
 pub enum TypeVariableValue {
@@ -115,25 +95,6 @@ impl TypeVariableValue {
             TypeVariableValue::Known { .. } => false,
         }
     }
-
-    // pub(crate) fn maybe_unify(&self, other: &Self) -> Result<Self, InferenceError> {
-    //     match (self, other) {
-    //         // We never equate two type variables, both of which
-    //         // have known types. Instead, we recursively equate
-    //         // those types.
-    //         (&TypeVariableValue::Known { .. }, &TypeVariableValue::Known { .. }) => {
-    //             unreachable!("equating two type variables, both of which have known types")
-    //         }
-    //         // If one side is known, prefer that one.
-    //         (&TypeVariableValue::Known { .. }, &TypeVariableValue::Unknown) => Ok(value1.clone()),
-    //         (&TypeVariableValue::Unknown, &TypeVariableValue::Known { .. }) => Ok(value1.clone()),
-
-    //         // both unknown, doesn't matter
-    //         (&TypeVariableValue::Unknown, &TypeVariableValue::Unknown) => {
-    //             Ok(TypeVariableValue::Unknown)
-    //         }
-    //     }
-    // }
 }
 
 // the poly type
@@ -195,13 +156,6 @@ impl ConstraintCtx {
             kind: ConstraintKind::Eq(lhs, rhs),
         });
     }
-
-    // pub fn unify_var_ty(&mut self, e: ExprId, lhs: TyId, rhs: Ty<TyId>) {
-    //     self.constraints.push(Constraint {
-    //         location: e,
-    //         kind: ConstraintKind::Eq(lhs, rhs),
-    //     });
-    // }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -415,147 +369,6 @@ impl<'db> CheckCtx<'db> {
         }
 
         set
-    }
-}
-
-/// Traverse the table and freeze all `Ty`s into immutable ones.
-struct Collector<'db> {
-    cache: HashMap<TyId, ArcTy>,
-    ctx: CheckCtx<'db>,
-}
-
-impl<'db> Collector<'db> {
-    fn new(ctx: CheckCtx<'db>) -> Self {
-        Self {
-            cache: HashMap::with_capacity(ctx.table.len()),
-            ctx,
-        }
-    }
-
-    fn finalize_inference(&mut self) -> InferenceResult {
-        let ctx = &mut self.ctx;
-
-        let name_tys: Vec<_> = ctx
-            .module
-            .names()
-            .map(|(name, _)| {
-                // TODO: i think this is causing a new instantiation?
-                // let ty = ctx.ty_for_name(name);
-                // dbg!(&(name, name_txt));
-                let ty = u32::from(name.into_raw()).into();
-                (name, ty)
-            })
-            .collect();
-
-        let expr_tys: Vec<_> = ctx
-            .module
-            .exprs()
-            .map(|(expr, _)| {
-                let ty = ctx.ty_for_expr(expr);
-                (expr, ty)
-            })
-            .collect();
-
-        let name_cnt = ctx.module.names().len();
-        let expr_cnt = ctx.module.exprs().len();
-        let mut name_ty_map = HashMap::with_capacity(name_cnt);
-        let mut expr_ty_map = HashMap::with_capacity(expr_cnt);
-        for (name, ty) in name_tys {
-            name_ty_map.insert(name, self.canonicalize_type(ty));
-        }
-        for (expr, ty) in expr_tys {
-            expr_ty_map.insert(expr, self.canonicalize_type(ty));
-        }
-
-        // dbg!(&self.ctx.table);
-
-        InferenceResult {
-            name_ty_map,
-            expr_ty_map,
-        }
-    }
-
-    fn canonicalize_type(&mut self, ty: TyId) -> ArcTy {
-        let i = self.ctx.table.find(ty);
-        if let Some(ty) = self.cache.get(&i).cloned() {
-            return ty;
-        }
-
-        let ret = self.canonicalize_type_uncached(ty);
-        self.cache.insert(i, ret.clone());
-        ret
-    }
-
-    fn canonicalize_type_uncached(&mut self, ty_id: TyId) -> ArcTy {
-        // let ty = self.ctx.get_ty(ty_id);
-
-        let ty = self.ctx.table.inlined_probe_value(ty_id).known();
-
-        let ty = if let Some(t) = ty {
-            t
-        } else {
-            let root = self.ctx.table.find(ty_id);
-            // eprintln!("{ty_id:?} is unknown\troot: {root:?}");
-            Ty::TyVar(root.0)
-        };
-
-        match ty {
-            Ty::TyVar(x) => {
-                // TODO: this should just be a generic param at this point
-                // should eventually normalize the vars so they start from 0
-                // could maybe do that as a final pass on the name?
-                ArcTy::TyVar(x)
-                // if x != ty_id.0 {
-                //     self.canonicalize_type(TyId::from(x))
-                // } else {
-                //     ArcTy::TyVar(x)
-                // }
-            }
-            Ty::List(inner_id) => {
-                let c_inner = self.canonicalize_type(inner_id);
-                ArcTy::List(c_inner.into())
-            }
-            Ty::Lambda { param, body } => {
-                let c_param = self.canonicalize_type(param).into();
-                let c_body = self.canonicalize_type(body).into();
-                ArcTy::Lambda {
-                    param: c_param,
-                    body: c_body,
-                }
-            }
-            Ty::AttrSet(attr_set_ty) => {
-                // let c_attr = self.canonicalize_attrset(attr_set_ty);
-                // ArcTy::AttrSet(c_attr)
-                // ArcTy::AttrSet(AttrSetTy {
-                //     fields: BTreeMap::new(),
-                //     dyn_ty: None,
-                //     rest: None,
-                // })
-                self.canonicalize_attrset(attr_set_ty)
-            }
-            Ty::Primitive(p) => ArcTy::Primitive(p),
-        }
-    }
-
-    fn canonicalize_attrset(&mut self, attr_set_ty: AttrSetTy<TyId>) -> ArcTy {
-        let mut new_fields = BTreeMap::<SmolStr, TyRef>::new();
-        for (k, &v_id) in &attr_set_ty.fields {
-            let field_ty = self.canonicalize_type(v_id).into();
-            new_fields.insert(k.clone(), field_ty);
-        }
-        let c_dyn_ty = attr_set_ty
-            .dyn_ty
-            .map(|d_id| self.canonicalize_type(d_id).into());
-        // TODO: merge this in with fields
-        let c_rest = attr_set_ty
-            .rest
-            .map(|r_id| self.canonicalize_type(r_id).into());
-
-        ArcTy::AttrSet(AttrSetTy {
-            fields: new_fields,
-            dyn_ty: c_dyn_ty,
-            rest: c_rest,
-        })
     }
 }
 
