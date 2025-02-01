e5b21b29e7a2ce6f424f81ff555006d70752143b
John Murray <5672686+JRMurr@users.noreply.github.com>
2025-02-01 14:58:49 -0500

    update pbt script

--- pbt.sh
+++ pbt.sh
@@ -5,4 +5,4 @@ DEFAULT_PROPTEST_CASES=50000
 # allow passing in cases as arg
 PROPTEST_CASES=${1:-$DEFAULT_PROPTEST_CASES}
 
-PROPTEST_CASES=$PROPTEST_CASES cargo test --package lang --lib -- ty::check::pbt::test_type_check --exact --show-output
+PROPTEST_CASES=$PROPTEST_CASES cargo test --package lang_check --lib -- pbt::test_type_check --exact --show-output 
